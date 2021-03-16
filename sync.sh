fa8f386f49e025bec4a3da0dfb0fa701f1f0908e
qlijin <qlijin@gmail.com>
2021-03-16 14:57:42 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker pull k8s.gcr.io/kube-apiserver:v1.18.16
 docker tag k8s.gcr.io/kube-apiserver:v1.18.16 registry.cn-beijing.aliyuncs.com/qlijin/lijinrepo/kube-apiserver:v1.18.16
 docker push registry.cn-beijing.aliyuncs.com/qlijin/lijinrepo/kube-apiserver:v1.18.16
